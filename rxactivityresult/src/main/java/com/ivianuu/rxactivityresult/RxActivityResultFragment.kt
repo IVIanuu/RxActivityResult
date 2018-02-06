a0217f0cbd6a7ba145abaaa9bb7e98194c8bd42b
IVIanuu <IVIanuu@gmail.com>
2018-02-06 21:04:12 +0100

    Fixed a leak

--- rxactivityresult/src/main/java/com/ivianuu/rxactivityresult/RxActivityResultFragment.kt
+++ rxactivityresult/src/main/java/com/ivianuu/rxactivityresult/RxActivityResultFragment.kt
@@ -62,23 +62,12 @@ class RxActivityResultFragment : Fragment(), ActivityResultStarter, Application.
         return startForResult(intent, options)
     }
 
-    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
-    }
-
-    override fun onActivityStarted(activity: Activity?) {
-    }
-
-    override fun onActivityPaused(activity: Activity?) {
-    }
-
-    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
-    }
-
-    override fun onActivityResumed(activity: Activity?) {
-    }
-
-    override fun onActivityStopped(activity: Activity?) {
-    }
+    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {}
+    override fun onActivityStarted(activity: Activity?) {}
+    override fun onActivityPaused(activity: Activity?) {}
+    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {}
+    override fun onActivityResumed(activity: Activity?) {}
+    override fun onActivityStopped(activity: Activity?) {}
 
     override fun onActivityDestroyed(activity: Activity?) {
         if (this.act == activity) {

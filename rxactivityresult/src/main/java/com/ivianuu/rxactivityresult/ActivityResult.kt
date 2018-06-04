08cacc42ce9fc107ac2f3a1a6351ac989e8b8dde
IVIanuu <IVIanuu@gmail.com>
2018-06-04 16:06:37 +0200

    Updated rxjava

--- rxactivityresult/src/main/java/com/ivianuu/rxactivityresult/ActivityResult.kt
+++ rxactivityresult/src/main/java/com/ivianuu/rxactivityresult/ActivityResult.kt
@@ -22,9 +22,11 @@ import android.content.Intent
 /**
  * Represents an activity result
  */
-data class ActivityResult(val requestCode: Int,
-                          val resultCode: Int,
-                          val data: Intent?) {
+data class ActivityResult(
+    val requestCode: Int,
+    val resultCode: Int,
+    val data: Intent?
+) {
 
     val isOk: Boolean
         get() = resultCode == Activity.RESULT_OK
